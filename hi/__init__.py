9fdd55c9ba64d229e22ea726a407b4aa0bef2f35
femto <femtowin@gmail.com>
2023-08-12 15:28:55 +0800

    first commit

--- hi/__init__.py
+++ hi/__init__.py
@@ -46,11 +46,24 @@ def main():
         run_command()
 
 def run_file(hiscript_path):
-    global f
+
     with open(hiscript_path, 'r') as f:
         hiscript = f.read()
     # Execute from cache if we have a hit
     script_name = os.path.basename(hiscript_path)
+
+    execute(hiscript, script_name)
+def run_command():
+
+
+        hiscript = ' '.join(sys.argv[1:])
+        # Execute from cache if we have a hit
+        script_name = '-'.join(sys.argv[1:])
+
+        execute(hiscript, script_name)
+
+
+def execute(hiscript, script_name):
     script_hash = hashlib.sha256(hiscript.encode()).hexdigest()
     script_cache_path = os.path.join(CACHE_DIR, f"{script_name}-{script_hash}")
     if HI_REGENERATE == "true" and os.path.exists(script_cache_path):
@@ -64,19 +77,19 @@ def run_file(hiscript_path):
         sys.exit()
     # System prompt
     system_prompt = """You are humanscript, a human readable script interpreter, here are your rules:
-You read an input script consisting of human readable commands and convert it to a bash output script.
-You always start the output script with the exact shebang \"#!/usr/bin/env bash\".
-You take care to provide portable code that runs well on both macos and linux systems.
-Lines in the input script starting with a '#' are comments.
-You NEVER respond with markdown, only respond in pure bash.
-You NEVER explain anything about the script.
-"""
+    You read an input script consisting of human readable commands and convert it to a bash output script.
+    You always start the output script with the exact shebang \"#!/usr/bin/env bash\".
+    You take care to provide portable code that runs well on both macos and linux systems.
+    Lines in the input script starting with a '#' are comments.
+    You NEVER respond with markdown, only respond in pure bash.
+    You NEVER explain anything about the script.
+    """
     # User prompt
     user_prompt = f"""
-### Input script:
-{hiscript}
+    ### Input script:
+    {hiscript}
 
-### Output script:"""
+    ### Output script:"""
     # Format JSON payload
     data_payload = {
         "model": HI_MODEL,
@@ -95,97 +108,34 @@ You NEVER explain anything about the script.
     }
     response = requests.post(f"{HI_API}/chat/completions", headers=headers, data=json.dumps(data_payload))
     if response.status_code == 200:
-        response_data = response.json()
-        output_script = response_data.get('choices', [{}])[0].get('delta', {}).get('content', "")
+        lines = response.text.split('\n')
+        processed_lines = []
+
+        for line in lines:
+            # 去掉以 "data: " 开头的部分
+            if line.startswith("data: "):
+                line = line.replace("data: ", "", 1)
+            if not line.startswith("[DONE]") and not line.strip() == '':
+                processed_lines.append(line)
+
+        # 将处理后的行合并回文本
+
+        output_script = [json.loads(line).get('choices', [{}])[0].get('delta', {}).get('content', "") for line in
+                         processed_lines]
+        script = ''.join(output_script)
+
         with open(script_cache_path, 'w') as f:
-            f.write(output_script)
+
+            # print("writing to file: " + script_cache_path)
+            f.write(script)
         if HI_EXECUTE == "true":
-            subprocess.run(['bash', '-s'] + sys.argv[2:], input=output_script.encode())
+
+            subprocess.run(['bash', '-s'] + sys.argv[2:], input=script.encode())
+
         else:
             print(output_script)
     else:
         print(f"Error: {response.text}")
-def run_command():
-
-
-        hiscript = ' '.join(sys.argv[1:])
-        # Execute from cache if we have a hit
-        script_name = os.path.basename('-'.join(sys.argv[1:]))
-        script_hash = hashlib.sha256(hiscript.encode()).hexdigest()
-        script_cache_path = os.path.join(CACHE_DIR, f"{script_name}-{script_hash}")
-        if HI_REGENERATE == "true" and os.path.exists(script_cache_path):
-            os.remove(script_cache_path)
-        if os.path.exists(script_cache_path):
-            if HI_EXECUTE == "true":
-                subprocess.run(['bash', script_cache_path] + sys.argv[2:])
-            else:
-                with open(script_cache_path, 'r') as f:
-                    print(f.read())
-            sys.exit()
-        # System prompt
-        system_prompt = """You are humanscript, a human readable script interpreter, here are your rules:
-    You read an input script consisting of human readable commands and convert it to a bash output script.
-    You always start the output script with the exact shebang \"#!/usr/bin/env bash\".
-    You take care to provide portable code that runs well on both macos and linux systems.
-    Lines in the input script starting with a '#' are comments.
-    You NEVER respond with markdown, only respond in pure bash.
-    You NEVER explain anything about the script.
-    """
-        # User prompt
-        user_prompt = f"""
-    ### Input script:
-    {hiscript}
-
-    ### Output script:"""
-        # Format JSON payload
-        data_payload = {
-            "model": HI_MODEL,
-            "messages": [
-                {"role": "system", "content": system_prompt},
-                {"role": "user", "content": user_prompt}
-            ],
-            "temperature": 0.7,
-            "max_tokens": 1000,
-            "stream": True
-        }
-        # Send request and handle the response
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization": f"Bearer {HI_API_KEY}"
-        }
-
-        response = requests.post(f"{HI_API}/chat/completions", headers=headers, data=json.dumps(data_payload))
-        if response.status_code == 200:
-            lines = response.text.split('\n')
-            processed_lines = []
-
-            for line in lines:
-                # 去掉以 "data: " 开头的部分
-                if line.startswith("data: "):
-                    line = line.replace("data: ", "", 1)
-                if not line.startswith("[DONE]") and not line.strip() == '':
-                    processed_lines.append(line)
-
-
-            # 将处理后的行合并回文本
-
-            output_script = [json.loads(line).get('choices', [{}])[0].get('delta', {}).get('content', "") for line in processed_lines]
-            script = ''.join(output_script)
-
-            with open(script_cache_path, 'w') as f:
-
-                #print("writing to file: " + script_cache_path)
-                f.write(script)
-            if HI_EXECUTE == "true":
-
-                process = subprocess.run(['bash', '-s'] + sys.argv[2:], input=script.encode(), stdout=subprocess.PIPE)
-                output = process.stdout.decode()
-                print(output, end="")
-            else:
-                print(output_script)
-        else:
-            print(f"Error: {response.text}")
-
 
 
 if __name__ == "__main__":
